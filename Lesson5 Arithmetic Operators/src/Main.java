bdf65b7d3293feadb89c85a03733df33389cf80a
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2025-09-17 21:03:38 +0700

    Add files via upload

---
+++ Lesson5 Arithmetic Operators/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+//        z = x % y;
+//        System.out.println(z);
+    }
+}

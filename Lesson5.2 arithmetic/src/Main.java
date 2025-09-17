6466e24d32d65f378c69a4041b2d05fd80800ce7
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2025-09-17 21:03:52 +0700

    Add files via upload

---
+++ Lesson5.2 arithmetic/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Augmented Assignment Operators
+
+        int x = 10;
+        int y = 2;
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+        x %= y;
+        System.out.println(x);
+    }
+}

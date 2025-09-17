2fb73dc95fca43df342c7f44ff6eaca9fe8f1e34
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2025-09-17 21:04:21 +0700

    Add files via upload

---
+++ Lesson5.4 arithmetic/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // ORDER OF OPERATIONS [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+    }
+}

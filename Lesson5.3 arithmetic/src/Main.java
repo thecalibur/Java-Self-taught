bcb627e84368cc00e4d9e86fc5701407182a561d
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2025-09-17 21:04:07 +0700

    Add files via upload

---
+++ Lesson5.3 arithmetic/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Increment and Decrement Operator
+
+        int x = 1;
+
+//        x += 1;
+//        x++;
+//        x--;
+
+
+        System.out.println(x);
+    }
+}
